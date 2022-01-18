664ffc4607511211257026e25d9df550415ed9b2
namnam2303 <85600809+namnam2303@users.noreply.github.com>
2022-01-18 23:49:50 +0700

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,41 +1,69 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.List;
+import java.util.Scanner;
 
-public class GamePanel extends JPanel implements Runnable {
-    final int WIDTH = 910;
-    final int HEIGHT = 600;
-    final int UNIT_SIZE = 30;
-    char direction = 'R';
+public class GamePanel extends JPanel implements ActionListener{
+    static final int WIDTH = 900;
+    static final int HEIGHT = 600;
+    static final int UNIT_SIZE = 30;
+    static final int DELAY = 110;
     List<Integer> snakeBodyX;
     List<Integer> snakeBodyY;
-    boolean running;
     int bodyParts = 3;
+    int highScore;
     int applesEaten = 0;
     int appleX;
     int appleY;
-    int highScore;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
     Random random;
-    Thread thread;
-    int speed = 100;
 
-    public GamePanel() {
+    GamePanel(){
+        highScore = getHighScore();
         random = new Random();
         snakeBodyX = new ArrayList<>();
         snakeBodyY = new ArrayList<>();
-        this.setFocusable(true);
         setInitialSnake();
-        highScore = getHighScore();
-        running = true;
-        newApple();
+        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
-        thread = new Thread(this);
-        thread.start();
+        startGame();
+    }
+    private int getHighScore() {
+        File file = new File("highscore.txt");
+        String score = "0";
+        try {
+            Scanner myScanner = new Scanner(file);
+            while (myScanner.hasNextLine()) {
+                score = myScanner.nextLine();
+            }
+            if (applesEaten > Integer.parseInt(score) && !running) {
+                upgradeScore(file);
+            }
+            myScanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return Integer.parseInt(score);
+    }
+    private void upgradeScore(File file) {
+        PrintWriter myPrint = null;
+        try {
+            myPrint = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        myPrint.println(applesEaten);
+        myPrint.close();
     }
 
     private void setInitialSnake() {
@@ -45,6 +73,44 @@ public class GamePanel extends JPanel implements Runnable {
         snakeBodyY.add(0);
         snakeBodyX.add(0);
         snakeBodyY.add(0);
+    }
+    private void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        for (int i = 1; i < bodyParts; i++) {
+            g.setColor(new Color(0, 143, 50));
+            g.fillRect(snakeBodyX.get(i), snakeBodyY.get(i), UNIT_SIZE, UNIT_SIZE);
+        }
+        g.setColor(Color.green);
+        g.fillRect(snakeBodyX.get(0), snakeBodyY.get(0), UNIT_SIZE, UNIT_SIZE);
+        g.setColor(Color.red);
+        Font myFont = new Font("Serif", Font.BOLD, 30);
+        g.setFont(myFont);
+        g.drawString("Score : " + applesEaten, 30, 40);
+        g.drawString("High score: " + highScore, 675, 40);
+    }
+
+    private void newApple() {
+        appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+        while (snakeBodyY.contains(appleY) || snakeBodyX.contains(appleX) || appleY >= HEIGHT - (2 * UNIT_SIZE) || appleX >= WIDTH - (2 * UNIT_SIZE)) {
+            appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
+            appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+        }
     }
 
     private void move() {
@@ -66,7 +132,17 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
 
-    private void checkCollision() {
+    private void eatenApple() {
+        if (snakeBodyY.get(0) == appleY && snakeBodyX.get(0) == appleX) {
+            bodyParts++;
+            snakeBodyX.add(snakeBodyX.get(bodyParts - 3));
+            snakeBodyY.add(snakeBodyY.get(bodyParts - 3));
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    private void checkCollisions() {
         int snakeHeadX = snakeBodyX.get(0);
         int snakeHeadY = snakeBodyY.get(0);
         for (int i = 1; i < bodyParts; i++) {
@@ -76,21 +152,21 @@ public class GamePanel extends JPanel implements Runnable {
         }
         switch (direction) {
             case 'U':
-                if (snakeBodyY.get(0) - UNIT_SIZE < 0) {
+                if (snakeBodyY.get(0) < 0) {
                     gameOver();
                 }
                 break;
             case 'D':
-                if (snakeBodyY.get(0) + UNIT_SIZE >= HEIGHT - UNIT_SIZE) {
+                if (snakeBodyY.get(0) == HEIGHT - UNIT_SIZE) {
                     gameOver();
                 }
                 break;
             case 'L':
-                if (snakeBodyX.get(0) - UNIT_SIZE < 0) {
+                if (snakeBodyX.get(0) < 0) {
                     gameOver();
                 }
             case 'R':
-                if (snakeBodyX.get(0) == WIDTH - UNIT_SIZE) {
+                if (snakeBodyX.get(0) == WIDTH) {
                     gameOver();
                 }
         }
@@ -106,91 +182,15 @@ public class GamePanel extends JPanel implements Runnable {
         System.exit(0);
     }
 
-    private int getHighScore() {
-        File file = new File("highscore.txt");
-        String score = "0";
-        try {
-            Scanner myScanner = new Scanner(file);
-            while (myScanner.hasNextLine()) {
-                score = myScanner.nextLine();
-            }
-            if (applesEaten > Integer.parseInt(score) && !running) {
-                upgradeScore(file);
-            }
-                myScanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return Integer.parseInt(score);
-    }
-    private void upgradeScore(File file) {
-        PrintWriter myPrint = null;
-        try {
-            myPrint = new PrintWriter(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        myPrint.println(applesEaten);
-        myPrint.close();
-    }
-
-
-    private void newApple() {
-        appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
-        appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
-        while (snakeBodyY.contains(appleY) || snakeBodyX.contains(appleX) || appleY >= HEIGHT - (2 * UNIT_SIZE) || appleX >= WIDTH - (2 * UNIT_SIZE)) {
-            appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
-            appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
-        }
-    }
-
-
     @Override
-    public void paint(Graphics g) {
-        g.setColor(Color.black);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-        g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-        for (int i = 1; i < bodyParts; i++) {
-            g.setColor(new Color(0, 143, 50));
-            g.fillRect(snakeBodyX.get(i), snakeBodyY.get(i), UNIT_SIZE, UNIT_SIZE);
+    public void actionPerformed(ActionEvent e) {
+        if(running) {
+            move();
+            eatenApple();
+            checkCollisions();
         }
-        g.setColor(Color.green);
-        g.fillRect(snakeBodyX.get(0), snakeBodyY.get(0), UNIT_SIZE, UNIT_SIZE);
-        g.setColor(Color.red);
-        Font myFont = new Font("Serif", Font.BOLD, 30);
-        g.setFont(myFont);
-        g.drawString("Score : " + applesEaten, 30, 40);
-        g.drawString("High score: " + highScore, 675, 40);
+        repaint();
     }
-
-    private void eatenApple() {
-        if (snakeBodyY.get(0) == appleY && snakeBodyX.get(0) == appleX) {
-            bodyParts++;
-            snakeBodyX.add(snakeBodyX.get(bodyParts - 3));
-            snakeBodyY.add(snakeBodyY.get(bodyParts - 3));
-            applesEaten++;
-            newApple();
-            if (applesEaten % 5 == 0) {
-                speed -=10;
-            }
-        }
-    }
-
-    @Override
-    public void run() {
-        while (running) {
-            try {
-                checkCollision();
-                move();
-                eatenApple();
-                repaint();
-                Thread.sleep(speed);
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
-
     private class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
