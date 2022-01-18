e35aba4aa260307adfcaa97af8c2ecece4254b93
namnam2303 <85600809+namnam2303@users.noreply.github.com>
2022-01-18 23:57:23 +0700

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,7 +9,7 @@ import java.util.Random;
 import java.util.List;
 import java.util.Scanner;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel2 extends JPanel implements ActionListener{
     static final int WIDTH = 900;
     static final int HEIGHT = 600;
     static final int UNIT_SIZE = 30;
@@ -26,7 +26,7 @@ public class GamePanel extends JPanel implements ActionListener{
     Timer timer;
     Random random;
 
-    GamePanel(){
+    GamePanel2(){
         highScore = getHighScore();
         random = new Random();
         snakeBodyX = new ArrayList<>();
@@ -117,10 +117,34 @@ public class GamePanel extends JPanel implements ActionListener{
         int x = snakeBodyX.get(0);
         int y = snakeBodyY.get(0);
         switch (direction) {
-            case 'U' -> snakeBodyY.set(0, snakeBodyY.get(0) - UNIT_SIZE);
-            case 'D' -> snakeBodyY.set(0, snakeBodyY.get(0) + UNIT_SIZE);
-            case 'L' -> snakeBodyX.set(0, snakeBodyX.get(0) - (UNIT_SIZE));
-            case 'R' -> snakeBodyX.set(0, snakeBodyX.get(0) + UNIT_SIZE);
+            case 'U' -> {
+                if (snakeBodyY.get(0) == 0) {
+                    snakeBodyY.set(0, HEIGHT - (UNIT_SIZE * 2));
+                } else {
+                    snakeBodyY.set(0, snakeBodyY.get(0) - UNIT_SIZE);
+                }
+            }
+            case 'D' -> {
+                if (snakeBodyY.get(0) == HEIGHT - (UNIT_SIZE * 2)) {
+                    snakeBodyY.set(0, 0);
+                } else {
+                    snakeBodyY.set(0, snakeBodyY.get(0) + UNIT_SIZE);
+                }
+            }
+            case 'L' -> {
+                if (snakeBodyX.get(0) < 0) {
+                    snakeBodyX.set(0, WIDTH - UNIT_SIZE);
+                } else  {
+                    snakeBodyX.set(0, snakeBodyX.get(0) - (UNIT_SIZE));
+                }
+            }
+            case 'R' -> {
+                if (snakeBodyX.get(0) == WIDTH - UNIT_SIZE) {
+                    snakeBodyX.set(0, 0);
+                }  else {
+                    snakeBodyX.set(0, snakeBodyX.get(0) + UNIT_SIZE);
+                }
+            }
         }
         for (int i = 1; i < bodyParts; i++) {
             int x2 = snakeBodyX.get(i);
@@ -149,26 +173,6 @@ public class GamePanel extends JPanel implements ActionListener{
             if (snakeHeadX == snakeBodyX.get(i) && snakeHeadY == snakeBodyY.get(i)) {
                 gameOver();
             }
-        }
-        switch (direction) {
-            case 'U':
-                if (snakeBodyY.get(0) < 0) {
-                    gameOver();
-                }
-                break;
-            case 'D':
-                if (snakeBodyY.get(0) == HEIGHT - UNIT_SIZE) {
-                    gameOver();
-                }
-                break;
-            case 'L':
-                if (snakeBodyX.get(0) < 0) {
-                    gameOver();
-                }
-            case 'R':
-                if (snakeBodyX.get(0) == WIDTH) {
-                    gameOver();
-                }
         }
     }
 
