bbc4bf5b6c362d5b63b80fe22033d9b41459afa2
namnam2303 <imsofh@gmail.com>
2024-07-21 22:33:06 +0700

    the final version : synchronized list accessing

--- src/Mode1.java
+++ src/Mode1.java
@@ -30,8 +30,9 @@ public class Mode1 extends JPanel implements ActionListener {
     ExecutorService executor;
 
     private final Object fileLock = new Object();
+    private final Object listLock = new Object();
 
-    Mode1() {
+    public Mode1() {
         executor = Executors.newSingleThreadExecutor();
         highScore = getHighScore();
         random = new Random();
@@ -52,12 +53,14 @@ public class Mode1 extends JPanel implements ActionListener {
     }
 
     private void setInitialSnake() {
-        snakeBodyX.add(UNIT_SIZE * 2);
-        snakeBodyY.add(0);
-        snakeBodyX.add(UNIT_SIZE);
-        snakeBodyY.add(0);
-        snakeBodyX.add(0);
-        snakeBodyY.add(0);
+        synchronized (listLock) {
+            snakeBodyX.add(UNIT_SIZE * 2);
+            snakeBodyY.add(0);
+            snakeBodyX.add(UNIT_SIZE);
+            snakeBodyY.add(0);
+            snakeBodyX.add(0);
+            snakeBodyY.add(0);
+        }
     }
 
     private void startGame() {
@@ -77,12 +80,14 @@ public class Mode1 extends JPanel implements ActionListener {
         g.fillRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-        for (int i = 1; i < bodyParts; i++) {
-            g.setColor(new Color(0, 143, 50));
-            g.fillRect(snakeBodyX.get(i), snakeBodyY.get(i), UNIT_SIZE, UNIT_SIZE);
+        synchronized (listLock) {
+            for (int i = 1; i < bodyParts; i++) {
+                g.setColor(new Color(0, 143, 50));
+                g.fillRect(snakeBodyX.get(i), snakeBodyY.get(i), UNIT_SIZE, UNIT_SIZE);
+            }
+            g.setColor(Color.green);
+            g.fillRect(snakeBodyX.get(0), snakeBodyY.get(0), UNIT_SIZE, UNIT_SIZE);
         }
-        g.setColor(Color.green);
-        g.fillRect(snakeBodyX.get(0), snakeBodyY.get(0), UNIT_SIZE, UNIT_SIZE);
         g.setColor(Color.red);
         Font myFont = new Font("Serif", Font.BOLD, 30);
         g.setFont(myFont);
@@ -93,88 +98,105 @@ public class Mode1 extends JPanel implements ActionListener {
     private void newApple() {
         appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
         appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
-        while (snakeBodyY.contains(appleY) || snakeBodyX.contains(appleX) || appleY >= HEIGHT - (2 * UNIT_SIZE) || appleX >= WIDTH - (2 * UNIT_SIZE)) {
-            appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
-            appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+        synchronized (listLock) {
+            while (snakeBodyY.contains(appleY) || snakeBodyX.contains(appleX) || appleY >= HEIGHT - (2 * UNIT_SIZE) || appleX >= WIDTH - (2 * UNIT_SIZE)) {
+                appleX = random.nextInt(WIDTH / UNIT_SIZE) * UNIT_SIZE;
+                appleY = random.nextInt(HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+            }
         }
     }
 
     private void move() {
-        int x = snakeBodyX.get(0);
-        int y = snakeBodyY.get(0);
-        switch (direction) {
-            case 'U' -> snakeBodyY.set(0, snakeBodyY.get(0) - UNIT_SIZE);
-            case 'D' -> snakeBodyY.set(0, snakeBodyY.get(0) + UNIT_SIZE);
-            case 'L' -> snakeBodyX.set(0, snakeBodyX.get(0) - (UNIT_SIZE));
-            case 'R' -> snakeBodyX.set(0, snakeBodyX.get(0) + UNIT_SIZE);
-        }
-        for (int i = 1; i < bodyParts; i++) {
-            int x2 = snakeBodyX.get(i);
-            snakeBodyX.set(i, x);
-            x = x2;
-            int y2 = snakeBodyY.get(i);
-            snakeBodyY.set(i, y);
-            y = y2;
+        synchronized (listLock) {
+            int x = snakeBodyX.get(0);
+            int y = snakeBodyY.get(0);
+            switch (direction) {
+                case 'U' -> snakeBodyY.set(0, snakeBodyY.get(0) - UNIT_SIZE);
+                case 'D' -> snakeBodyY.set(0, snakeBodyY.get(0) + UNIT_SIZE);
+                case 'L' -> snakeBodyX.set(0, snakeBodyX.get(0) - UNIT_SIZE);
+                case 'R' -> snakeBodyX.set(0, snakeBodyX.get(0) + UNIT_SIZE);
+            }
+            for (int i = 1; i < bodyParts; i++) {
+                int x2 = snakeBodyX.get(i);
+                snakeBodyX.set(i, x);
+                x = x2;
+                int y2 = snakeBodyY.get(i);
+                snakeBodyY.set(i, y);
+                y = y2;
+            }
         }
     }
 
     private void eatenApple() {
-        if (snakeBodyY.get(0) == appleY && snakeBodyX.get(0) == appleX) {
-            bodyParts++;
-            snakeBodyX.add(snakeBodyX.get(bodyParts - 3));
-            snakeBodyY.add(snakeBodyY.get(bodyParts - 3));
-            applesEaten++;
-            newApple();
-            if (applesEaten % 5 == 0) {
-                delay -= 10;
-                timer.setDelay(delay);
+        synchronized (listLock) {
+            if (snakeBodyY.get(0) == appleY && snakeBodyX.get(0) == appleX) {
+                bodyParts++;
+                snakeBodyX.add(snakeBodyX.get(bodyParts - 3));
+                snakeBodyY.add(snakeBodyY.get(bodyParts - 3));
+                applesEaten++;
+                newApple();
+                if (applesEaten % 5 == 0) {
+                    delay -= 10;
+                    timer.setDelay(delay);
+                }
             }
         }
     }
 
     private void checkCollisions() {
-        int snakeHeadX = snakeBodyX.get(0);
-        int snakeHeadY = snakeBodyY.get(0);
-        for (int i = 1; i < bodyParts; i++) {
-            if (snakeHeadX == snakeBodyX.get(i) && snakeHeadY == snakeBodyY.get(i)) {
-                gameOver();
+        synchronized (listLock) {
+            int snakeHeadX = snakeBodyX.get(0);
+            int snakeHeadY = snakeBodyY.get(0);
+            for (int i = 1; i < bodyParts; i++) {
+                if (snakeHeadX == snakeBodyX.get(i) && snakeHeadY == snakeBodyY.get(i)) {
+                    gameOver();
+                }
             }
-        }
-        switch (direction) {
-            case 'U':
-                if (snakeBodyY.get(0) < 0) {
-                    gameOver();
-                }
-                break;
-            case 'D':
-                if (snakeBodyY.get(0) == HEIGHT - UNIT_SIZE) {
-                    gameOver();
-                }
-                break;
-            case 'L':
-                if (snakeBodyX.get(0) < 0) {
-                    gameOver();
-                }
-            case 'R':
-                if (snakeBodyX.get(0) == WIDTH) {
-                    gameOver();
-                }
+            switch (direction) {
+                case 'U':
+                    if (snakeBodyY.get(0) < 0) {
+                        gameOver();
+                    }
+                    break;
+                case 'D':
+                    if (snakeBodyY.get(0) == HEIGHT - UNIT_SIZE) {
+                        gameOver();
+                    }
+                    break;
+                case 'L':
+                    if (snakeBodyX.get(0) < 0) {
+                        gameOver();
+                    }
+                case 'R':
+                    if (snakeBodyX.get(0) == WIDTH) {
+                        gameOver();
+                    }
+            }
         }
     }
 
     private void gameOver() {
         running = false;
+        timer.stop();
         executor.execute(() -> {
             synchronized (fileLock) {
                 if (getHighScore() < applesEaten) {
                     writeFile("highscore.txt", String.valueOf(applesEaten));
-                    SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(this, "New high score: " + applesEaten, "Congratulations!", JOptionPane.INFORMATION_MESSAGE));
+                    SwingUtilities.invokeLater(() -> showGameOverMessage("New high score: " + applesEaten, "Congratulations!"));
                 } else {
-                    SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(this, "YOUR SCORE: " + applesEaten, "Game over", JOptionPane.INFORMATION_MESSAGE));
+                    SwingUtilities.invokeLater(() -> showGameOverMessage("YOUR SCORE: " + applesEaten, "Game over"));
                 }
             }
         });
-        System.exit(0);
+    }
+
+    private void showGameOverMessage(String message, String title) {
+        JOptionPane.showMessageDialog(this, message, title, JOptionPane.INFORMATION_MESSAGE);
+        SwingUtilities.invokeLater(() -> {
+            JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+            topFrame.dispose();
+            new Home();
+        });
     }
 
     private String readFile(String fileName, String defaultValue) {
